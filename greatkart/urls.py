50966ac7bd7211017514dbf08f3acd92bcc7ceab
cumironin <cumironin@gmail.com>
2022-06-24 17:52:01 +0700

    first commit djangocommerce

---
+++ greatkart/urls.py
@@ -0,0 +1,14 @@
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("store/", include("store.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
